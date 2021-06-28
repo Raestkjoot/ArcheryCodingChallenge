1afe3af7133ddecd30ff282c2d6b38adf235d09a
TANNGRISNIR\Gunnar Magnussen <gunnarmagnum@gmail.com>
2021-06-28 03:41:20 +0200

    Main menu added

---
+++ ArcheryCodingChallenge/Assets/Scripts/Menu/Play.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public Renderer rend;
+    public Material hoverMaterial;
+    public Material downMaterial;
+    private Material normalMaterial;
+
+    void Start() {
+       normalMaterial = rend.material;
+    }
+    void OnMouseEnter() {
+        rend.material = hoverMaterial;
+    }
+    void OnMouseExit() {
+        rend.material = normalMaterial;
+    }
+    void OnMouseOver() {
+        if(Input.GetButtonDown("Fire1")){
+            rend.material = downMaterial;
+        }
+        if(Input.GetButtonUp("Fire1")){
+            rend.material = hoverMaterial;
+            SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        }
+    }
+}
